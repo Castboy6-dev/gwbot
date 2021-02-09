2a2038864f06803f5d4b01fef89ed1e06d896f8b
Castboy6-dev <77079729+Castboy6-dev@users.noreply.github.com>
2021-02-09 10:00:50 -0800

    Update help.js

--- commands.js/help.js
+++ commands.js/help.js
@@ -10,13 +10,13 @@ module.exports.run = async (client, message, args) => {
       .setAuthor("Zero")
       .setTitle("Command List & Guide for the Bot")
       .setDescription("Below are Commands you can do with Bot, Right now there is only 6 commands available, more commands will be added soon.")
-      .addField("🎉 Giveaway 🎉","start [channel-name] [Time] [winners] [Prize]\nreroll [prize name]\nend [prize name]")
+      .addField("🎁 Giveaway 🎁","start [channel-name] [Time] [winners] [Prize]\nreroll [prize name]\nend [prize name]")
       .addField("Examples", "g!start #giveaway 5m 1 Testing\ng!end Testing\ng!reroll Testing")
       .addField("Utility", "ping, invite", true)
-      .addField("â„¹ Information â„¹", "stats", true)
+      .addField("ℹ Information ℹ", "stats", true)
       .setTimestamp()
       .setFooter(`Command Requested By ${message.author.tag}`, client.user.displayAvatarURL());
-    message.channel.send("**Sent the commands in Direct Messages! ðŸ’Œ, Check DMs**");
+    message.channel.send("**Sent the commands in Direct Messages! 💌, Check DMs**");
 
     return message.author.send(help);
 }
